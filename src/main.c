2761f0bc049ad51b1976ef181cb1a96fbc1dd54d
zegervdv <zegervdv@me.com>
2014-03-15 18:18:50 +0100

    Fix USART communication
    Basic USART communication via USART3 operational
    TODO: connect BT module and work out communication protocol

--- src/main.c
+++ src/main.c
@@ -8,11 +8,63 @@
 #include "stm32f30x_gpio.h"
 #include "stm32f30x_usart.h"
 #include "stm32f3_discovery.h"
+#include "stm32f30x_misc.h"
 
-/* Function prototypes */
-void GPIOC_Init(void);
-void USART3_Init(void);
-void led_toggle(void);
+/*************************************************
+ * Initialize USART3 with interrupts
+ *************************************************/
+void USART3_Init(void)
+{
+  USART_InitTypeDef usart3_init_struct;
+  GPIO_InitTypeDef gpioc_init_struct;
+  NVIC_InitTypeDef NVIC_init_struct;
+
+  /* RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART3 | RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOC, ENABLE); */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+
+  /* Configure PIN10 as Tx, PIN11 as Rx */
+  gpioc_init_struct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+  gpioc_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+  gpioc_init_struct.GPIO_Mode = GPIO_Mode_AF;
+  gpioc_init_struct.GPIO_OType = GPIO_OType_PP;
+  gpioc_init_struct.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOC, &gpioc_init_struct);
+
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_7);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_7);
+
+
+  usart3_init_struct.USART_BaudRate = 9600;
+  usart3_init_struct.USART_WordLength = USART_WordLength_8b;
+  usart3_init_struct.USART_StopBits = USART_StopBits_1;
+  usart3_init_struct.USART_Parity = USART_Parity_No;
+  usart3_init_struct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  usart3_init_struct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART3, &usart3_init_struct);
+
+  USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+  
+  NVIC_init_struct.NVIC_IRQChannel = USART3_IRQn;
+  NVIC_init_struct.NVIC_IRQChannelPreemptionPriority = 0;// this sets the priority group of the USART1 interrupts
+	NVIC_init_struct.NVIC_IRQChannelSubPriority = 0;		 // this sets the subpriority inside the group
+	NVIC_init_struct.NVIC_IRQChannelCmd = ENABLE;			 // the USART1 interrupts are globally enabled
+	NVIC_Init(&NVIC_init_struct);
+
+  /* Enable USART3 */
+  USART_Cmd(USART3, ENABLE);
+}
+
+
+/*********************************************
+ * Sample - LED toggle
+ ********************************************/
+void led_toggle(void)
+{
+  STM_EVAL_LEDToggle(LED3);
+}
+
 
 int main(void)
 {
@@ -22,98 +74,17 @@ int main(void)
   STM_EVAL_LEDOff(LED4);
   STM_EVAL_LEDToggle(LED4);
 
-  /* Initialize GPIO PIN */
-  /* GPIOC_Init(); */
   /* Initialize USART3 */
-  /* USART3_Init(); */
+  USART3_Init();
   
   /* Start by sending first character */
-  /* USART_SendData(USART3, 't'); */
-
   while(1)
   {
     /* Loop and wait for interrupts */
+    int i = 10000000;
+    while(i--); 
+    USART_SendData(USART3, 0x74);
   }
-}
-
-/************************************************
- * Initialize GPIO Pin for output
-************************************************/
-void GPIOC_Init(void)
-{
-  GPIO_InitTypeDef gpioc_init_struct;
-  gpioc_init_struct.GPIO_Pin = GPIO_Pin_7;
-  gpioc_init_struct.GPIO_Mode = GPIO_Mode_OUT;
-  gpioc_init_struct.GPIO_OType = GPIO_OType_PP;
-  gpioc_init_struct.GPIO_PuPd = GPIO_PuPd_UP;
-  gpioc_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-  GPIO_Init(GPIOC, &gpioc_init_struct);
-  GPIO_SetBits(GPIOC, GPIO_Pin_7);
-}
-
-/*************************************************
- * Initialize USART3 with interrupts
- *************************************************/
-void USART3_Init(void)
-{
-  USART_InitTypeDef usart3_init_struct;
-  GPIO_InitTypeDef gpioc_init_struct;
-
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_7);
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_7);
-
-  /* RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART3 | RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOC, ENABLE); */
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-
-  /* Configure PIN10 as Tx */
-  gpioc_init_struct.GPIO_Pin = GPIO_Pin_10;
-  gpioc_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-  gpioc_init_struct.GPIO_Mode = GPIO_Mode_AF;
-  gpioc_init_struct.GPIO_OType = GPIO_OType_PP;
-  gpioc_init_struct.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOC, &gpioc_init_struct);
-
-  /* Configure PIN11 as Rx */
-  gpioc_init_struct.GPIO_Pin = GPIO_Pin_11;
-  gpioc_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-  gpioc_init_struct.GPIO_Mode = GPIO_Mode_IN;
-  gpioc_init_struct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOC, &gpioc_init_struct);
-
-  /* Enable USART3 */
-  USART_Cmd(USART3, ENABLE);
-
-  usart3_init_struct.USART_BaudRate = 9600;
-  usart3_init_struct.USART_WordLength = USART_WordLength_8b;
-  usart3_init_struct.USART_StopBits = USART_StopBits_1;
-  usart3_init_struct.USART_Parity = USART_Parity_No;
-  usart3_init_struct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-  usart3_init_struct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-
-  USART_Init(USART3, &usart3_init_struct);
-
-  USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
-
-  NVIC_EnableIRQ(USART1_IRQn);
-}
-
-/*********************************************
- * Sample - LED toggle
- ********************************************/
-void led_toggle(void)
-{
-  uint8_t led_bit = GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_7);
-
-  STM_EVAL_LEDToggle(LED3);
-  /* if(led_bit == (uint8_t)Bit_SET) */
-  /* { */
-  /*   GPIO_ResetBits(GPIOC, GPIO_Pin_7); */
-  /* } */
-  /* else */
-  /* { */
-  /*   GPIO_SetBits(GPIOC, GPIO_Pin_7); */
-  /* } */
 }
 
 /*******************************************
@@ -121,12 +92,19 @@ void led_toggle(void)
  ******************************************/
 void USART3_IRQHandler(void)
 {
-  if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET )
+  if(USART_GetITStatus(USART3, USART_IT_RXNE))
   {
-    if((char)USART_ReceiveData(USART3) == 't')
-    {
-      led_toggle();
-      USART_SendData(USART3, 't');
-    }
+		char t = USART3->RDR; // the character from the USART1 data register is saved in t
+    
+    // Toggle LED3 on reception of data
+    led_toggle();
+
+		if(t == 'a'){ 
+      // If received data is "a", toggle LED4
+      STM_EVAL_LEDToggle(LED4);
+		}
+		else{ 
+      
+		}
   }
 }
