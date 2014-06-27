9c73080e397af884792b6bc0ad5f88c3f76f6996
zegervdv <zegervdv@me.com>
2014-06-27 11:20:43 +0200

    Fix error in read sensors

--- debug/read_sensors.c
+++ debug/read_sensors.c
@@ -6,7 +6,8 @@
 
 #include "sensor_data.h"
 
-#define BUFFERSIZE 255
+#define DATASIZE 20
+#define BUFFERSIZE (10*DATASIZE)
 
 int main(int argc, char const* argv[]) {
   struct termios tio;
@@ -15,8 +16,9 @@ int main(int argc, char const* argv[]) {
   unsigned char c;
   unsigned char k[BUFFERSIZE];
   int i = 0;
-  float value[3], val;
-  unsigned char data[18];
+  float val;
+  unsigned char data[DATASIZE];
+  int mark = 0;
 
   // struct sensor_data data;
 
@@ -56,27 +58,38 @@ int main(int argc, char const* argv[]) {
       printf("\n");
   }
 
+  for (i = 0; i < BUFFERSIZE; i++) {
+    if (mark > 0) {
+      data[DATASIZE - mark] = k[i];
+      mark--;
+    }else {
+      if (k[i + 1] == 0x55 && k[i + 2] == 0x55)
+        mark = DATASIZE;
+    }
+  }
+
   printf("\n\n");
 
-  for (i = 0; i < 18; i++)
-    data[i] = k[i+2];
+  for (i = 0; i < DATASIZE; i++)
+    printf("%02X ", data[i]);
+  printf("\n");
 
   printf("Gyroscope:\n");
-  for (i = 0; i < 3; i++) {
-    value[i] = (float) ((int)(((unsigned int) data[2*i+1] << 8) + data[2*i])) / L3G_Sensitivity_500dps;
-    printf("%f dps\n", value[i]);
+  for (i = 1; i < 4; i++) {
+    val = (float) ((int)(((unsigned int) data[2*i+1] << 8) + data[2*i])) / L3G_Sensitivity_500dps;
+    printf("%f dps\n", val);
   }
 
   printf("Compass:\n");
-  for (i = 3; i < 5; i++) {
+  for (i = 4; i < 6; i++) {
     val = (float)((int)(((unsigned int)data[2*i] << 8) + data[2*i+1])*1000)/ LSM303DLHC_M_SENSITIVITY_XY_8_1Ga;
     printf("%f Ga\n", val);
   }
-  val = (float)((int)(((unsigned int)data[10] << 8) + data[11])*1000)/ LSM303DLHC_M_SENSITIVITY_Z_8_1Ga;
+  val = (float)((int)(((unsigned int)data[12] << 8) + data[13])*1000)/ LSM303DLHC_M_SENSITIVITY_Z_8_1Ga;
   printf("%f Ga\n", val);
 
   printf("Accelerometer:\n");
-  for (i = 6; i < 9; i++) {
+  for (i = 7; i < 10; i++) {
     int raw = (int)((((unsigned int)data[2*i + 1] << 8) + data[2*i]) >> 4);
     val = (float)raw/LSM_Acc_Sensitivity_2g;
     printf("%f mg\n", val);
