6a20accabc85f9c0f4040d3832d00ec6f5a4bea0
zegervdv <zegervdv@me.com>
2014-07-26 18:31:30 +0200

    Finish initialization code for PWM timers

--- source/motors.c
+++ source/motors.c
@@ -7,7 +7,7 @@
 void motors_init(void) {
   // Select TIMER1 on pins PA8, PA9, PA10 and PA11
   TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-  TIM_OC_InitTypeDef TIM_OCInitStructure;
+  TIM_OCInitTypeDef TIM_OCInitStructure;
   GPIO_InitTypeDef GPIO_InitStructure;
 
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
@@ -21,9 +21,44 @@ void motors_init(void) {
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+  // Select the Alternate Function TIM1 for pins
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_6);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_6);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_6);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource11, GPIO_AF_6);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource11, GPIO_AF_11);
+
+  // Set TIM1 scaler
+  TIM_TimeBaseStructure.TIM_Prescaler = PWM_PRESCALER;
+  TIM_TimeBaseStructure.TIM_Period = PWM_PERIOD;
+  TIM_TimeBaseStructure.ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+  // Initialize Output Comparators
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = PWM_PULSE / 2;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
+  TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+  TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
+  TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Set;
+
+  // Initalize channels
+  TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+  TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+  TIM_OC2Init(TIM1, &TIM_OCInitStructure);
+  TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+  TIM_OC3Init(TIM1, &TIM_OCInitStructure);
+  TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+  TIM_OC4Init(TIM1, &TIM_OCInitStructure);
+  TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+  TIM_ARRPreloadConfig(TIM1, DISABLE);
+  TIM_CtrlPWMOutputs(TIM1, ENABLE);
+  TIM_Cmd(TIM1, ENABLE);
 }
 
--- source/motors.h
+++ source/motors.h
@@ -6,7 +6,43 @@
 #include "stm32f30x_tim.h"
 
 /**
+ * System clock runs at 72MHz
+ * Set TIMER frequency at 2MHz
+ * Prescaler = SYS_CLK / TIM_FREQ = 36
+ * PWM frequency at 200 Hz
+ * PWM period = TIM_FREQ / PWM_FREQ = 10000
+ * PWM pulse length = 2000 ms
+ */
+#define SYS_CLK       (uint32_t)(72e6)
+#define TIM_FREQ      (uint32_t)(2e6)
+#define PWM_PRESCALER (uint32_t)(SYS_CLK / TIM_FREQ)
+#define PWM_FREQ      (uint32_t)(200)
+#define PWM_PERIOD    (uint32_t)(TIM_FREQ / PWM_FREQ)
+#define PWM_PULSE     (uint32_t)(PWM_PERIOD / (1000.0/PWM_FREQ))
+
+/**
+ * Identifiers for motors
+ */
+#define MOTOR_LEFT_FRONT    (uint8_t)(0x1)
+#define MOTOR_RIGHT_FRONT   (uint8_t)(0x2)
+#define MOTOR_LEFT_BACK     (uint8_t)(0x4) 
+#define MOTOR_RIGHT_BACK    (uint8_t)(0x8)
+
+
+/**
  * Initialize timers for PWM control
  */
 void motors_init(void);
 
+
+/**
+ * Set PWM duty cycle
+ * TODO: Use TIM_SetCompare<CHANNEL>();
+ * motor - Select one or more motors to set:
+ *         MOTOR_LEFT_FRONT
+ *         MOTOR_RIGHT_FRONT
+ *         MOTOR_LEFT_BACK
+ *         MOTOR_RIGHT_BACK
+ * speed - value of speed to be set
+ */
+void motors_set_speed(uint8_t motor, uint16_t speed);
