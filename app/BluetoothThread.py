39866e3f1decd0e95392b466fa2d043860c95312
xerxer <xer.xer@hotmail.com>
2015-01-03 16:35:18 +0100

    Fixed bluetooth blocking issues

--- app/BluetoothThread.py
+++ app/BluetoothThread.py
@@ -14,8 +14,10 @@ class BluetoothThread(QThread):
         self.ser=serial.Serial()
         self.runThread=True
         self.databuffer=[]
+        self.timer=0
 
     def send(self):
+
         if self.ser.isOpen():
             
             while len(self.sendbuf)>0:
@@ -44,7 +46,7 @@ class BluetoothThread(QThread):
               
                 for d in data:
                     self.ser.write(d)
-                    time.sleep(0.05)
+                    #time.sleep(0.05)
 
     def initialize(self,port):
         self.runThread=True
@@ -60,18 +62,17 @@ class BluetoothThread(QThread):
         self.ser.bytesize = serial.EIGHTBITS #number of bits per bytes
         self.ser.parity = serial.PARITY_NONE #set parity check: no parity
         self.ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-        self.ser.timeout = None          #block read
+        self.ser.timeout = 0          #block read
         self.ser.xonxoff = False     #disable software flow control
         self.ser.rtscts = False     #disable hardware (RTS/CTS) flow control
         self.ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-        self.ser.writeTimeout = 2     #timeout for write
+        self.ser.writeTimeout = 0     #timeout for write
 
         self.ser.close()
         while not self.ser.isOpen():
             try:
                 self.ser.close()
                 self.ser.open()
-                self.timestamp=time.time()
                 self.start()
             except Exception, e:
                 print "error open serial port: " + str(e)
@@ -80,7 +81,10 @@ class BluetoothThread(QThread):
     def run(self):
         while self.runThread:
             
-            a=self.ser.read()       
+            a=self.ser.read() 
+            if not a:
+                continue
+
             self.databuffer.append(a)
             
             if len(self.databuffer)<22:
@@ -88,6 +92,13 @@ class BluetoothThread(QThread):
             
             if self.databuffer[0]=='U' and self.databuffer[1]=='U':
                 
+                helper=time.time()
+                if helper-self.timer>0.04:
+                    self.timer=helper
+                else:
+                    self.databuffer=[]
+                    continue
+                    
                 self.databuffer=self.databuffer[2:]
                 for i in range(len(self.databuffer)):
                     self.receivebuf.append(self.databuffer.pop(0))
