adecd9db43df5aa66e8676bba3fb5f1f3d202a5b
zegervdv <zegervdv@me.com>
2015-07-11 18:09:36 +0200

    Fix timer
    Solve an issue which prevented TIM3 to raise an interrupt

--- source/system.c
+++ source/system.c
@@ -9,39 +9,27 @@ void TIM_init() {
   TIM_OCInitTypeDef  TIM_OCInitStructure;
   NVIC_InitTypeDef NVIC_InitStructure;
 
-  /* Compute the prescaler value */
-  uint16_t PrescalerValue = (uint16_t)((SystemCoreClock) / 72000000) - 1;
-
   /* TIM3 clock enable */
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-  /* Enable the TIM3 gloabal Interrupt */
+  /* Enable the TIM3 global Interrupt */
   NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
 
+  NVIC_EnableIRQ(TIM3_IRQn);
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+
   /* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = 65535;
-  TIM_TimeBaseStructure.TIM_Prescaler = 0;
-  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_Period = 10000 - 1;
+  TIM_TimeBaseStructure.TIM_Prescaler = 72000 - 1; // 1 kHz
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
   TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-
-  /* Prescaler configuration */
-  TIM_PrescalerConfig(TIM3, PrescalerValue, TIM_PSCReloadMode_Immediate);
-
-  /* Output Compare Timing Mode configuration: Channel1 */
-  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-  TIM_OCInitStructure.TIM_Pulse = 40961;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-
-  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
-
-  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Disable);
+  TIM_Cmd(TIM3, ENABLE);
 }
 
 void quadcopter_init(void) {
